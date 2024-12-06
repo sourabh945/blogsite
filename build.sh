6281f930d658bcb9bb17e64b613583c989bfb449
sourabh945 <sourabhsheokand945@gmail.com>
2024-12-07 00:23:08 +0530

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-py manage.py makemigrations core
+python manage.py makemigrations core
 
 # Convert static asset files
 python manage.py collectstatic --no-input
