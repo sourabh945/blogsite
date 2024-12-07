db7949cfa4d6fe51ae5c8ec61ff1644b8bcd0260
sourabh945 <sourabhsheokand945@gmail.com>
2024-12-07 14:22:13 +0530

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -17,19 +17,6 @@ python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Start the Django development server (optional, for local dev environment)
-echo "Starting Django development server..."
-python manage.py runserver &
 
-# Start the Celery worker
-echo "Starting Celery worker..."
-celery -A your_project_name worker --loglevel=info &
-
-# Optionally, you can also start the Celery beat scheduler if you need periodic tasks
-# echo "Starting Celery beat scheduler..."
-# celery -A your_project_name beat --loglevel=info &
-
-# Wait for background processes to finish (you can change this logic depending on your deployment setup)
-wait
 
 
